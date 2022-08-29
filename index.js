f5f739ee7be5359d375aa07a15a6b68ff8d10eaf
Ahmed Alli <muniirhaddadi1@gmail.com>
2022-08-29 08:24:18 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Your code here
+
+
+let dataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300],
+    ["Byron", "Poodle", "Mascot", 3],
+    ["Julius", "Caesar", "General", 27],
+    ["Rafiki", "", "Aide", 10],
+    ["Simba", "", "King", 100]
+  ]
+
+ function createEmployeeRecord(row){
+    let employeeRecord = 
+    {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord 
+    
+  
+ }
+
+ let createEmployeeRecords = function(employeeData) {
+   return employeeData.map(function(row){
+       return createEmployeeRecord(row)
+   })
+}
+ createEmployeeRecords()
+
+
+ function createTimeInEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+ }
+ 
+function createTimeOutEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+
+}
+
+ function hoursWorkedOnDate(){
+
+ }
+
+function wagesEarnedOnDate(){
+
+}
+ function calculatePayroll()
